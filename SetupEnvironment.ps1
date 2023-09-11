db60ccf28549d197824de70be166b79dd60a62eb
Takahiro Tada <t.tada@kms3.com>
2023-09-11 20:28:20 +0900

    Add sql server search

--- SetupEnvironment.ps1
+++ SetupEnvironment.ps1
@@ -8,6 +8,16 @@ $env:OPENAI_LLM_DEPLOYMENT_NAME = ""
 $env:OPENAI_LLM_MODEL_NAME = ""
 $env:OPENAI_EMBEDDING_DEPLOYMENT_NAME = ""
 $env:OPENAI_EMBEDDING_MODEL_NAME = ""
+$env:OPENAI_API_VERSION = ""
+$env:OPENAI_LLM_TEMPERATURE = ""
+
+$env:SQL_SERVER_HOST = ""
+$env:SQL_SERVER_PORT = ""
+$env:SQL_SERVER_DATABASE = ""
+$env:SQL_SERVER_USER_NAME = ""
+$env:SQL_SERVER_PASSWORD = ""
+$env:SQL_SERVER_DRIVER_NAME = "ODBC Driver 17 for SQL Server"
+$env:SQL_SERVER_INCLUDE_TABLES = ""
 
 if (!(Test-Path $venvDir)) {
     python -m venv $venvDir
